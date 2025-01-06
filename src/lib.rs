eedff705a4a8a0804e6989f8916d2a0b0e666f24
Lee Fowler <fowler.lee8@gmail.com>
2025-01-06 23:50:10 +0100

    commit setup

---
+++ src/lib.rs
@@ -0,0 +1,15 @@
+pub fn write_to_page() -> bool {
+    
+	return true;
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn test_page_write() {
+        let result = write_to_page();
+        assert_eq!(result, true);
+    }
+}
