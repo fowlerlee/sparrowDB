85c08219668af5475786fa085bc5be8fe29b1859
Lee Fowler <fowler.lee8@gmail.com>
2025-01-08 21:30:27 +0100

    impl cache for lru

--- src/buffer/bufferpoolmanager.rs
+++ src/buffer/bufferpoolmanager.rs
@@ -2,10 +2,19 @@ use std::collections::HashMap;
 use std::sync::atomic::AtomicUsize;
 use std::sync::{Arc, Mutex};
 
-type PageId = u64;
+type PageId = usize;
+type FrameId = usize;
 
 #[derive(Copy, Clone, Debug, PartialEq)]
 struct FrameHeader {}
+
+#[derive(Debug, Default, Clone, PartialEq)]
+struct LRUNode {
+    history: Vec<usize>,
+    k_: usize,
+    fid: FrameId,
+    is_evictable: bool,
+}
 
 // Evict() -> std::optional<frame_id_t> : Evict the frame that has the largest backward k-distance compared to all other evictable frames being tracked by the Replacer. If there are no evictable frames, return std::nullopt.
 // RecordAccess(frame_id_t frame_id) : Record that the given frame has been accessed at the current timestamp. This method should be called after a page has been pinned in the BufferPoolManager.
@@ -13,29 +22,63 @@ struct FrameHeader {}
 // SetEvictable(frame_id_t frame_id, bool set_evictable) : This method controls whether a frame is evictable or not. It also controls the LRUKReplacer's size. You'll know when to call this function when you implement the BufferPoolManager. To be specific, when the pin count of a page hits 0, its corresponding frame should be marked as evictable.
 // Size() -> size_t : This method returns the number of evictable frames that are currently in the LRUKReplacer.
 
-#[derive(Copy, Clone, Debug, PartialEq, Default)]
-struct LRUKReplacer {}
+// [[maybe_unused]] std::unordered_map<frame_id_t, LRUKNode> node_store_;
+//   [[maybe_unused]] size_t current_timestamp_{0};
+//   [[maybe_unused]] size_t curr_size_{0};
+//   [[maybe_unused]] size_t replacer_size_;
+//   [[maybe_unused]] size_t k_;
+//   [[maybe_unused]] std::mutex latch_;
+
+#[derive(Clone, Debug, Default)]
+struct LRUKReplacer {
+    node_store: HashMap<FrameId, LRUNode>,
+    current_timestamp: usize,
+    current_size: usize,
+    replacer_size: usize,
+    k_: usize,
+    latch: Arc<Mutex<LRUNode>>,
+}
 
 impl LRUKReplacer {
-    fn new() -> Self {
+    fn new(&self) -> Self {
         Self::default()
     }
 
-    fn Evict(&self) {
-        eprint!("unimplemented");
+    fn Evict(&mut self) -> Option<FrameId> {
+        for (k, v) in self.node_store.iter() {
+            if v.is_evictable {
+                // v.history[2] = std::time::SystemTime::now();
+            }
+        }
+
+
+        return None;
+    }
+
+    fn RecordAccess(&self) {}
+
+    fn Remove(&mut self) {}
+
+    fn SetEvictable(&mut self, frame_id_t: FrameId, set_evictable: bool) {
+        if let Some(x) = self.node_store.get_mut(&frame_id_t){
+            x.is_evictable = set_evictable;
+        }
+    }
+
+    fn Size(&self) -> usize {
+        return 1 as usize;
     }
 }
 
-// NewPage() -> page_id_t
-// DeletePage(page_id_t page_id) -> bool
-// CheckedWritePage(page_id_t page_id) -> std::optional<WritePageGuard>
-// CheckedReadPage(page_id_t page_id) -> std::optional<ReadPageGuard>
-// FlushPage(page_id_t page_id) -> bool
-// FlushAllPages()
-// GetPinCount(page_id_t page_id)
-
 #[derive(Default)]
+struct Page {
+    id: usize,
+}
+
 pub struct BufferPoolManager {
+    num_frames: usize,
+    next_page: AtomicUsize,
+    bpm_latch: Arc<Mutex<Page>>,
     atomic_counter: AtomicUsize,
     latch: Arc<Mutex<()>>,
     frames: Vec<FrameHeader>,
@@ -47,6 +90,9 @@ pub struct BufferPoolManager {
 impl BufferPoolManager {
     pub fn new(capacity: usize) -> Self {
         Self {
+            num_frames: 0,
+            next_page: AtomicUsize::new(0),
+            bpm_latch: Arc::new(Mutex::new(Page::default())),
             atomic_counter: AtomicUsize::new(0),
             latch: Arc::new(Mutex::new(())),
             frames: Vec::new(),
@@ -59,6 +105,4 @@ impl BufferPoolManager {
     pub fn getBufferPoolSize(&self) -> usize {
         self.frames.len()
     }
-
-    
 }
