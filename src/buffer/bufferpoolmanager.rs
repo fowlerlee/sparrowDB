4f09913e0c08cb2fb4190abea739da3172696daf
Lee Fowler <fowler.lee8@gmail.com>
2025-01-08 19:24:51 +0100

    prep for the impl of cache and poolmanager

--- src/buffer/bufferpoolmanager.rs
+++ src/buffer/bufferpoolmanager.rs
@@ -7,6 +7,12 @@ type PageId = u64;
 #[derive(Copy, Clone, Debug, PartialEq)]
 struct FrameHeader {}
 
+// Evict() -> std::optional<frame_id_t> : Evict the frame that has the largest backward k-distance compared to all other evictable frames being tracked by the Replacer. If there are no evictable frames, return std::nullopt.
+// RecordAccess(frame_id_t frame_id) : Record that the given frame has been accessed at the current timestamp. This method should be called after a page has been pinned in the BufferPoolManager.
+// Remove(frame_id_t frame_id) : Clear all access history associated with a frame. This method should be called only when a page is deleted in the BufferPoolManager.
+// SetEvictable(frame_id_t frame_id, bool set_evictable) : This method controls whether a frame is evictable or not. It also controls the LRUKReplacer's size. You'll know when to call this function when you implement the BufferPoolManager. To be specific, when the pin count of a page hits 0, its corresponding frame should be marked as evictable.
+// Size() -> size_t : This method returns the number of evictable frames that are currently in the LRUKReplacer.
+
 #[derive(Copy, Clone, Debug, PartialEq, Default)]
 struct LRUKReplacer {}
 
@@ -19,6 +25,14 @@ impl LRUKReplacer {
         eprint!("unimplemented");
     }
 }
+
+// NewPage() -> page_id_t
+// DeletePage(page_id_t page_id) -> bool
+// CheckedWritePage(page_id_t page_id) -> std::optional<WritePageGuard>
+// CheckedReadPage(page_id_t page_id) -> std::optional<ReadPageGuard>
+// FlushPage(page_id_t page_id) -> bool
+// FlushAllPages()
+// GetPinCount(page_id_t page_id)
 
 #[derive(Default)]
 pub struct BufferPoolManager {
@@ -45,4 +59,6 @@ impl BufferPoolManager {
     pub fn getBufferPoolSize(&self) -> usize {
         self.frames.len()
     }
+
+    
 }
